da51d85aefbccb6c6d4f70234c094d5feaf35c0a
mirasoldavila13 <mirasoldavila13@aim.com>
2022-11-16 23:55:44 -0800

    Initial Commit

---
+++ Magic-8-Ball-IOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Magic-8-Ball-IOS
+//
+//  Created by Mirasol Davila on 11/16/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
